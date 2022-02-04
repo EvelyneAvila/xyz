147d377620f55d53f41e764b29de6592c270c396
EvelyneAvila <85718759+EvelyneAvila@users.noreply.github.com>
2022-02-04 12:50:39 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,8 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void AreaWithinTest() {
-
-        //expeted 219.1260838
         assertEquals(1, SkillDemo.RectangleArea(10, 5));
     }
 }
